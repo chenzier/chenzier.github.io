f78dc323ed5d39df042c6ffd47c7ffbe5d55b5b9
wangchen01 <chenwang01@dewu.com>
2026-09-07 15:32:41 +0800

    fix: 编辑此页链接指向本仓库

--- astro-paper.config.ts
+++ astro-paper.config.ts
@@ -24,7 +24,7 @@ export default defineAstroPaperConfig({
     showBackButton: true,
     editPost: {
       enabled: true,
-      url: "https://github.com/satnaing/astro-paper/edit/main/",
+      url: "https://github.com/chenzier/chenzier.github.io/edit/main/",
     },
     search: "pagefind",
   },
